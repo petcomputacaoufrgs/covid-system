e8fe8c07c3fd97587624c49beed5041b28d0e875
brunoczim <brunoczim@gmail.com>
2020-04-13 15:28:55 -0300

    ajeitando os testes

--- tests/NivelPrioridadeTest.php
+++ tests/NivelPrioridadeTest.php
@@ -1,10 +1,10 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-require_once __DIR__ . '/../classes/Banco/Banco.php';
-require_once __DIR__ . '/../classes/NivelPrioridade/NivelPrioridade.php';
-require_once __DIR__ . '/../classes/NivelPrioridade/NivelPrioridadeBD.php';
-require_once __DIR__ . '/../classes/NivelPrioridade/NivelPrioridadeRN.php';
+require_once 'classes/Banco/Banco.php';
+require_once 'classes/NivelPrioridade/NivelPrioridade.php';
+require_once 'classes/NivelPrioridade/NivelPrioridadeBD.php';
+require_once 'classes/NivelPrioridade/NivelPrioridadeRN.php';
 
 final class NivelPrioridadeTest extends TestCase {
     public function testObjeto() {
@@ -185,9 +185,7 @@ final class NivelPrioridadeTest extends TestCase {
 
         $this->assertTrue($tem_nivel);
     }
-    */
 
-    /*
     public function testPesquisarNaoAcha() {
         $rn = new NivelPrioridadeRN();
 
