43da0cc0229dca941cc24d4a361cae778b1d1c74
Joao Pedro SIlveira <jpedross1999@gmail.com>
2020-04-15 19:06:20 -0300

    Feat config database

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    'versao' => '1.0.0',
+    'producao' => false,
+    'banco' => array(
+        'servidor' => 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 
+        'nome' => 'ihnsqnls518k69nl',
+        'usuario' => 'frliar49dcb9ndg0',
+        'senha' => 'kzfk5tl458rvg070'
+    ),
+);
