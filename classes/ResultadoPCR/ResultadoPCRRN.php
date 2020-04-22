32f20856702c74c6a4713e7402cf98697ce8708f
Thiago <thiago.lermen@gmail.com>
2020-04-22 17:36:57 -0300

    added ResultadoPCRRN

--- classes/ResultadoPCR/ResultadoPCRRN.php
+++ classes/ResultadoPCR/ResultadoPCRRN.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../Excecao/Excecao.php';
+require_once __DIR__ . '/ResultadoPCR_BD.php';
 
 class ResultadoPCRRN
 {
@@ -23,4 +25,107 @@ class ResultadoPCRRN
         echo "Quencer ->" . $obj->getQuencher() . "<br>";
         echo "Ct ->" . $obj->getCt() . "<br>";
     }
+
+    public function cadastrar(ResultadoPCR $resultadoPCR) {
+        try {
+            
+            $objExcecao = new Excecao();
+            $objBanco = new Banco();
+            $objBanco->abrirConexao(); 
+            
+            /*Realizar validacoes*/
+
+            $objExcecao->lancar_validacoes();
+            $objResultadoPCR_BD = new ResultadoPCR_BD();
+            $objResultadoPCR_BD->cadastrar($resultadoPCR,$objBanco);
+            
+            $objBanco->fecharConexao();
+        } catch (Exception $e) {
+            throw new Excecao('Erro no cadastramento do resultado do PCR.', $e);
+        }
+    }
+
+    public function alterar(ResultadoPCR $resultadoPCR) {
+        try {  
+            $objExcecao = new Excecao();
+            $objBanco = new Banco();
+            $objBanco->abrirConexao(); 
+           
+            /*Realizar validacoes*/
+                       
+            $objExcecao->lancar_validacoes();
+            $objResultadoPCR_BD = new ResultadoPCR_BD();
+            $objResultadoPCR_BD->alterar($resultadoPCR,$objBanco);
+           
+           $objBanco->fecharConexao();
+       } catch (Exception $e) {
+           throw new Excecao('Erro na alteração do resultado do PCR.', $e);
+       }
+   }
+
+   public function consultar(ResultadoPCR $resultadoPCR) {
+        try {
+            $objExcecao = new Excecao();
+            $objBanco = new Banco();
+            $objBanco->abrirConexao(); 
+            $objExcecao->lancar_validacoes();
+            $objResultadoPCR_BD = new ResultadoPCR_BD();
+            $arr =  $objResultadoPCR_BD->consultar($resultadoPCR,$objBanco);
+            
+            $objBanco->fecharConexao();
+            return $arr;
+        } catch (Exception $e) {
+
+            throw new Excecao('Erro na consulta do resultado do PCR.',$e);
+        }
+    }
+
+    public function remover(ResultadoPCR $resultadoPCR) {
+        try {
+           $objExcecao = new Excecao();
+           $objBanco = new Banco();
+           $objBanco->abrirConexao(); 
+           $objExcecao->lancar_validacoes();
+           $objResultadoPCR_BD = new ResultadoPCR_BD();
+           $arr =  $objResultadoPCR_BD->remover($resultadoPCR,$objBanco);
+           $objBanco->fecharConexao();
+           return $arr;
+
+       } catch (Exception $e) {
+           throw new Excecao('Erro na remoção do resultado do PCR.', $e);
+       }
+   }
+
+   public function listar(ResultadoPCR $resultadoPCR) {
+        try {
+            $objExcecao = new Excecao();
+            $objBanco = new Banco();
+            $objBanco->abrirConexao(); 
+            $objExcecao->lancar_validacoes();
+            $objResultadoPCR_BD = new ResultadoPCR_BD();
+            
+            $arr = $objResultadoPCR_BD->listar($resultadoPCR,$objBanco);
+            
+            $objBanco->fecharConexao();
+            return $arr;
+        } catch (Exception $e) {
+            throw new Excecao('Erro na listagem do resultado do PCR.',$e);
+        }
+    }
+
+    public function pesquisar($campoBD, $valor_usuario) {
+        try {
+            $objExcecao = new Excecao();
+            $objBanco = new Banco();
+            $objBanco->abrirConexao(); 
+            $objExcecao->lancar_validacoes();
+            $objResultadoPCR_BD = new ResultadoPCR_BD();
+            $arr = $objResultadoPCR_BD->pesquisar($campoBD,$valor_usuario,$objBanco);
+            $objBanco->fecharConexao();
+            return $arr;
+        } catch (Exception $e) {
+            throw new Excecao('Erro na pesquisa do resultado do PCR.', $e);
+        }
+    }
+
 }
