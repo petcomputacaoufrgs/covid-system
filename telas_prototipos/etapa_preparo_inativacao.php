a0b8df42c59e8eb62bb9694bfd49732d38233bb2
Thiago Sotoriva Lermen <49798588+thiagolermen@users.noreply.github.com>
2020-04-25 16:10:01 -0300

    Update etapa_preparo_inativacao.php

--- telas_prototipos/etapa_preparo_inativacao.php
+++ telas_prototipos/etapa_preparo_inativacao.php
@@ -170,7 +170,7 @@ $objPagina = new Pagina();
   <head>
     <meta charset="utf-8">
     <meta name=viewport content="width=device-width, initial-scale=1">
-    <title>Login - Processo de tratamento de amostras para diagnóstico de COVID-19</title>
+    <title>Preparo e Inativação</title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
 
